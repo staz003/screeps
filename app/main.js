bed5e31037b5ea6f573cc87b29b80370071bc35b
Martin Roy <staz003@gmail.com>
2015-02-07 18:54:41 -0500

    No message

--- app/main.js
+++ app/main.js
@@ -1,21 +1,23 @@
+CURRENT_SOURCE = "current_source"
+spawn = Game.spawns.Spawn1
 
-
-if (!Game.spawns) {
-  console.log(Game.rooms)
+if (!Memory.spawns[spawn.name][CURRENT_SOURCE]) {
+  Memory.spawns[spawn.name][CURRENT_SOURCE] = spawn.pos.findClosest(Game.SOURCES)
 }
-if (Object.keys(Game.creeps).length === 0) {
-  Game.spawns.Spawn1.createCreep([Game.WORK, Game.CARRY, Game.MOVE], 'Worker1');
+
+if (Object.keys(Game.creeps).length < 3) {
+  spawn.createCreep([Game.WORK, Game.CARRY, Game.MOVE], undefined);
 }
 
 for (var i in Game.creeps) {
   var creep = Game.creeps[i]
   if(creep.energy < creep.energyCapacity) {
-    var sources = creep.room.find(Game.SOURCES);
+    var sources = spawn.pos.findClosest(Game.SOURCES);
     creep.moveTo(sources[0]);
     creep.harvest(sources[0]);
   }
   else {
-    creep.moveTo(Game.spawns.Spawn1);
-    creep.transferEnergy(Game.spawns.Spawn1)
+    creep.moveTo(spawn);
+    creep.transferEnergy(spawn)
   }
 }
