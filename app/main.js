04c6e6437efd96ffd96cc2d6df74bf21ae64010c
Martin Roy <staz003@gmail.com>
2015-02-07 16:34:31 -0500

    No message

--- app/main.js
+++ app/main.js
@@ -3,16 +3,16 @@
 if (!Game.spawns) {
   console.log(Game.rooms)
 }
-
-var creep = Game.creeps.Worker1;
-
-//#Howdy There!!!
-if(creep.energy < creep.energyCapacity) {
-	var sources = creep.room.find(Game.SOURCES);
-	creep.moveTo(sources[0]);
-	creep.harvest(sources[0]);
-}
-else {
-	creep.moveTo(Game.spawns.Spawn1);
-	creep.transferEnergy(Game.spawns.Spawn1)
-}
+//
+//var creep = Game.creeps.Worker1;
+//
+////#Howdy There!!!
+//if(creep.energy < creep.energyCapacity) {
+//	var sources = creep.room.find(Game.SOURCES);
+//	creep.moveTo(sources[0]);
+//	creep.harvest(sources[0]);
+//}
+//else {
+//	creep.moveTo(Game.spawns.Spawn1);
+//	creep.transferEnergy(Game.spawns.Spawn1)
+//}
