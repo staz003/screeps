bc8817669f6016047a12560090b9ee2b41d711aa
Martin Roy <staz003@gmail.com>
2015-02-07 16:17:38 -0500

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,11 @@
+var creep = Game.creeps.Worker1;
+
+if(creep.energy < creep.energyCapacity) {
+	var sources = creep.room.find(Game.SOURCES);
+	creep.moveTo(sources[0]);
+	creep.harvest(sources[0]);
+}
+else {
+	creep.moveTo(Game.spawns.Spawn1);
+	creep.transferEnergy(Game.spawns.Spawn1)
+}
