99fe28ee6998b53e0e871dead4de7af99f585303
Martin Roy <staz003@gmail.com>
2015-02-07 16:41:32 -0500

    No message

--- app/main.js
+++ app/main.js
@@ -3,16 +3,19 @@
 if (!Game.spawns) {
   console.log(Game.rooms)
 }
-//
-//var creep = Game.creeps.Worker1;
-//
-////#Howdy There!!!
-//if(creep.energy < creep.energyCapacity) {
-//	var sources = creep.room.find(Game.SOURCES);
-//	creep.moveTo(sources[0]);
-//	creep.harvest(sources[0]);
-//}
-//else {
-//	creep.moveTo(Game.spawns.Spawn1);
-//	creep.transferEnergy(Game.spawns.Spawn1)
-//}
+if (Object.keys(Game.creeps).length === 0) {
+  Game.spawns.Spawn1.createCreep([Game.WORK, Game.CARRY, Game.MOVE], 'Worker1');
+}
+
+for (var i in Game.creeps) {
+  creep = Game.creeps[i]
+  if(creep.energy < creep.energyCapacity) {
+    var sources = creep.room.find(Game.SOURCES);
+    creep.moveTo(sources[0]);
+    creep.harvest(sources[0]);
+  }
+  else {
+    creep.moveTo(Game.spawns.Spawn1);
+    creep.transferEnergy(Game.spawns.Spawn1)
+  }
+}
