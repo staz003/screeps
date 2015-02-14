77960ba9a7183284c0e1b3447bab5f281a52bd72
Martin Roy <staz003@gmail.com>
2015-02-13 20:40:08 -0500

    No message

--- app/team.js
+++ app/team.js
@@ -5,7 +5,8 @@ module.exports = function(spawn) {
   if (Object.keys(Game.creeps).length === 0) {
     spawn.createCreep([Game.WORK, Game.WORK, Game.WORK, Game.WORK, Game.MOVE], undefined, {role: "work"});
   } else if (Object.keys(Game.creeps).length === 1) {
-    //spawn.createCreep([Game.CARRY, Game.CARRY, Game.CARRY, Game.CARRY, Game.MOVE], undefined, {role: "carry"});
+    spawn.createCreep([Game.CARRY, Game.CARRY, Game.CARRY, Game.CARRY, Game.MOVE], undefined,
+      {role: "carry", teamate: Object.keys(Game.creeps)[0]});
   }
 
   for (var i in Game.creeps) {
@@ -13,6 +14,14 @@ module.exports = function(spawn) {
     if (creep.memory.role === "work") {
         creep.moveTo(source);
         creep.harvest(source);
+    } else if (creep.memory.role === "carry") {
+       if(creep.energy < creep.energyCapacity) {
+         creep.moveTo(Game.creeps[creep.memory.teamate]);
+         creep.pickup(creep.pos.findClosest(Game.DROPPED_ENERGY))
+       } else {
+         creep.moveTo(spawn);
+         creep.transferEnergy(spawn);
+       }
     }
   }
 }
